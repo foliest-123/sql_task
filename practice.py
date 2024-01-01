5f86ef66cdc0e26fee170130f8aadb4dbacc3dd9
foilest-123 <thegreatvijay1991@gmail.com>
2024-01-01 13:36:05 +0530

    sql orm completed

--- practice.py
+++ practice.py
@@ -1,4 +1,4 @@
-from sqlalchemy import URL, Column, Integer, Date, create_engine ,func , String , Float
+from sqlalchemy import URL, Column, Integer, Date, create_engine ,func , String , Float ,or_ , and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -9,6 +9,8 @@ Base = declarative_base()
 engine = create_engine(url)
 Session = sessionmaker(bind=engine)
 
+
+#customer Table
 class Order(Base):
     __tablename__ = 'orders'
 
@@ -25,6 +27,7 @@ class Order(Base):
         self.customer_id = customer_id
         self.salesman_id = salesman_id
 
+# salesman
 class Salesman(Base):
     __tablename__ = 'salesman'
 
@@ -37,6 +40,21 @@ class Salesman(Base):
         self.name = name
         self.city = city
         self.commission = commission
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    customer_id = Column(Integer, primary_key=True)
+    cust_name = Column(String(255))
+    city = Column(String(255))
+    grade = Column(Integer)
+    salesman_id = Column(Integer)
+
+    def __init__(self, customer_id, cust_name, city, grade, salesman_id):
+        self.customer_id = customer_id
+        self.cust_name = cust_name
+        self.city = city
+        self.grade = grade
+        self.salesman_id = salesman_id
 
 
 class EmployeeHistory(Base):
@@ -54,6 +72,33 @@ class EmployeeHistory(Base):
         self.end_date = end_date
         self.job_id = job_id
         self.department_id = department_id
+
+class Employee(Base):
+    __tablename__ = 'employee'
+
+    emp_idno = Column(Integer, primary_key=True)
+    emp_fname = Column(String(255))
+    emp_lname = Column(String(255))
+    emp_dept = Column(Integer)
+
+    def __init__(self, emp_idno, emp_fname, emp_lname, emp_dept):
+        self.emp_idno = emp_idno
+        self.emp_fname = emp_fname
+        self.emp_lname = emp_lname
+        self.emp_dept = emp_dept
+
+class Department(Base):
+    __tablename__ = 'department'
+
+    dpt_code = Column(Integer, primary_key=True)
+    dpt_name = Column(String(255))
+    dpt_allotment = Column(Integer)
+
+    def __init__(self, dpt_code, dpt_name, dpt_allotment):
+        self.dpt_code = dpt_code
+        self.dpt_name = dpt_name
+        self.dpt_allotment = dpt_allotment
+
 Base.metadata.create_all(engine)
 session = Session()
 
@@ -102,13 +147,81 @@ commission_range = session.query(Salesman.salesman_id,Salesman.commission).filte
 # b) Write a SQL query for the given table to retrieve avg details of commissions 
 # ranging from 0.12 to 0.14.(Begin and end values are included.)
 
-commision_avg = session.query(func.avg(Salesman.commission)).filter(Salesman.commission.between(0.12,0.14)).all()
+commision_avg = session.query(func.avg(Salesman.commission)).\
+                filter(Salesman.commission.between(0.12,0.14)).all()
 # print(commision_avg)
 
 # 3.
 # From the following table, write a SQL query to find those employees who 
 # worked more than or equal to  two jobs in the past. Return employee id. 
 
-more_than_twojob = session.query(EmployeeHistory.employee_id ,func.count(EmployeeHistory.job_id)).distinct().group_by(EmployeeHistory.employee_id).all()
-print(more_than_twojob)
+more_than_twojobs = session.query(EmployeeHistory.employee_id).distinct().\
+    group_by(EmployeeHistory.employee_id).\
+    having(func.count(EmployeeHistory.job_id) >= 2).\
+    all()
+# print(more_than_twojobs)
 
+# 4.a) Write a SQL statement to generate a list of salesmen who either serve one or 
+# more customers or have not joined any customer yet. The customers may have placed one or more orders 
+# with an order amount equal to or exceeding 2000, and they must have a grade. Alternatively, customers may 
+# not have placed any order with the associated supplier.(Use joins)
+# Return cust_name, cust city, grade, Salesman name, ord_no, ord_date, purch_amt
+
+
+# Customer.cust_name, Customer.city, Customer.grade,Salesman.name, Order.ord_no, Order.ord_date,max_purch_order.c.max_purch
+
+purch_amt_2000 =session.query(Customer.cust_name, Customer.city, Customer.grade,
+                             Salesman.name, Order.ord_no, Order.ord_date,
+                             func.max(Order.purch_amt)).\
+                            outerjoin(Customer,Customer.customer_id == Order.customer_id ).\
+                                outerjoin(Salesman , Salesman.salesman_id == Order.salesman_id)\
+                                    .group_by(Customer.customer_id).\
+                                    having(Customer.grade != None, func.max(Order.purch_amt >= 2000))
+                                 
+
+# for row in purch_amt_2000:
+#     print(row)
+    
+    
+# b) Write a SQL statement to generate a report with the customer name, city, order number, order date, and purchase amount for 
+# customers on the list who must have a grade and placed one or more orders. 
+# Additionally, include orders placed by customers who are neither on the 
+# list nor have a grade.(Use joins)
+
+
+distinct_cust_id = session.query(Order.customer_id).distinct().subquery()
+distinct_cust_ids = session.query(Order.customer_id)\
+    .distinct().subquery()
+
+placed_order =session.query(
+    Customer.cust_name,
+    Customer.city,
+    Order.ord_no,
+    Order.ord_date,
+    Order.purch_amt
+).outerjoin(Customer, Order.customer_id == Customer.customer_id).filter(
+    or_(
+        and_(Customer.grade != None, Order.customer_id.in_(distinct_cust_id)),
+        and_(Customer.grade == None, Order.customer_id.not_in(distinct_cust_id))
+    )
+)
+# for row in placed_order :
+#     print(row)
+    
+
+5. 
+# a) From the following tables write a SQL query to find those employees 
+# who work for the department where the departmental allotment amount is more
+# than Rs. 50000. Return emp_fname and emp_lname.
+
+allotment_dept = session.query(Employee.emp_fname , Employee.emp_lname).\
+    join(Department , Employee.emp_dept == Department.dpt_code).\
+        where(Department.dpt_allotment >= 50000).all()
+
+
+distinct_allotment = session.query(func.distinct(Department.dpt_allotment)).order_by(Department.dpt_allotment).slice(1, 2).subquery()
+
+second_lowamount = session.query(Employee.emp_fname , Employee.emp_lname).\
+    join(Department , Employee.emp_dept == Department.dpt_code).\
+        filter(Department.dpt_allotment == distinct_allotment).all()
+print(second_lowamount)
